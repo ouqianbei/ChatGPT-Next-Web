7cae130c716d08eb8d1d40a949fa0a377b0f6ab2
qianbei ou <ouqianbei@qq.com>
2023-05-11 01:19:30 +0000

    retitle

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "FOS TECH ChatGPT",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "FOS TECH ChatGPT",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
